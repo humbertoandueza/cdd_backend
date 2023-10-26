106a9dfb1537b85762e36cc960ed64acf8f541b0
humbertoandueza <humbertoanduezaa@gmail.com>
2023-10-26 13:53:33 -0400

    ngxn

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Paginator::useBootstrap();
         if (env('APP_ENV') == 'production') {
