0c82e09e966b897e847089a8c0feca6ebd19df79
humbertoandueza <humbertoanduezaa@gmail.com>
2023-10-26 15:14:51 -0400

    ngxn

--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,5 @@ Route::get('/stores', [StoreController::class, 'index'])->name('list-store');
 Route::post('/store/create', [StoreController::class, 'store'])->name('store-store');
 Route::put('/store/update/{id}', [StoreController::class, 'update'])->name('update-store');
 Route::get('/store/detail/{id}', [StoreController::class, 'show'])->name('detail-store');
-Route::get('/store/delete/{id}', [StoreController::class, 'delete'])->name('delete-store');
+Route::get('/store/delete/{id}', [StoreController::class, 'destroy'])->name('delete-store');
 Route::get('/store/count', [StoreController::class, 'create'])->name('count-store');
